aaaccf7d57701fe43d0ca790a7f99c0c89181e1a
ShastaloOleksii <shastalo.aleksej@tech-stack.io>
2020-02-11 12:10:43 +0200

    modified : firstTest, pom.xml and others files

--- src/test/java/com/automationpractice/firstTest.java
+++ src/test/java/com/automationpractice/firstTest.java
@@ -4,11 +4,12 @@ import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class firstTest {
+
     @Test
     public  void firstTest(){
+        System.setProperty("webdriver.chrome.driver", "C:/ChromeWebDriver/chromedriver.exe");
         ChromeDriver driver = new ChromeDriver();
-
         driver.get("http://automationpractice.com/index.php");
-        
+
     }
 }
